e97b624e034d5440d9ce73149b825daf14990393
Hyunji <guswl092@gmail.com>
2020-06-09 18:04:03 +0900

    GET All & Select and get & Insert Succeed!

--- NodeConnection_try1/app/src/main/java/hyunji/shin/nodeconnection_try1/MainActivity.java
+++ NodeConnection_try1/app/src/main/java/hyunji/shin/nodeconnection_try1/MainActivity.java
@@ -9,12 +9,10 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
 
-import org.json.JSONException;
 import org.json.JSONObject;
 
 import java.io.BufferedReader;
 import java.io.BufferedWriter;
-import java.io.Console;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
@@ -33,21 +31,39 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        tvData = (TextView)findViewById(R.id.textview);
-        Button btn = (Button)findViewById(R.id.button);
+        tvData = (TextView) findViewById(R.id.textview);
 
-        btn.setOnClickListener(new View.OnClickListener() {
-                                   @Override
-                                   public void onClick(View view) {
-                                       new JSONTask().execute("http://192.168.35.25:3306/person");
+        Button btn_all = (Button) findViewById(R.id.btn_all);
+        Button btn_post = (Button) findViewById(R.id.btn_post);
+        Button btn_insert = (Button) findViewById(R.id.btn_insert);
+
+        btn_all.setOnClickListener(new View.OnClickListener() { //GET ALL버튼을 누르면
+                                       @Override
+                                       public void onClick(View view) {
+                                           new JSONTask1().execute("http://192.168.35.25:3306/person");
+                                       }
                                    }
-                               }
         );
+        btn_post.setOnClickListener(new View.OnClickListener() { //POST버튼을 누르면
+            @Override
+            public void onClick(View view) {
+                new JSONTask2().execute("http://192.168.35.25:3306/post" ); //(1)이벤트 발생후 호출
+            }
+        });
+
+        btn_insert.setOnClickListener(new View.OnClickListener() { //insert DB 버튼을 누르면
+            @Override
+            public void onClick(View view) {
+                new JSONTask3().execute("http://192.168.35.25:3306/insert" ); //(1)이벤트 발생후 호출
+            }
+        });
+
     }
 
-    public class JSONTask extends AsyncTask<String,String,String> {
 
-        public JSONTask() {
+    public class JSONTask1 extends AsyncTask<String, String, String> {
+
+        public JSONTask1() {
             super();
         }
 
@@ -55,12 +71,6 @@ public class MainActivity extends AppCompatActivity {
         protected String doInBackground(String... urls) {
             try {
                 JSONObject jsonObject = new JSONObject();
-                jsonObject.accumulate("id", "11");
-                jsonObject.accumulate("name", "thisisname");
-                jsonObject.accumulate("address", "thisisaddress");
-                jsonObject.accumulate("email", "thisisemail");
-                jsonObject.accumulate("phone", "thisisphone");
-
                 HttpURLConnection con = null;
                 BufferedReader reader = null;
 
@@ -82,8 +92,8 @@ public class MainActivity extends AppCompatActivity {
                     String line = "";
 
                     //아래라인은 실제 reader에서 데이터를 가져오는 부분이다. 즉 node.js서버로부터 데이터를 가져온다.
-                    while((line = reader.readLine()) != null){
-                        Log.e("line",line);
+                    while ((line = reader.readLine()) != null) {
+                        Log.e("line", line);
                         buffer.append(line);
                     }
 
@@ -91,18 +101,18 @@ public class MainActivity extends AppCompatActivity {
                     return buffer.toString();
 
                     //아래는 예외처리 부분이다.
-                } catch (MalformedURLException e){
+                } catch (MalformedURLException e) {
                     e.printStackTrace();
                 } catch (IOException e) {
                     e.printStackTrace();
                 } finally {
                     //종료가 되면 disconnect메소드를 호출한다.
-                    if(con != null){
+                    if (con != null) {
                         con.disconnect();
                     }
                     try {
                         //버퍼를 닫아준다.
-                        if(reader != null){
+                        if (reader != null) {
                             reader.close();
                         }
                     } catch (IOException e) {
@@ -112,7 +122,7 @@ public class MainActivity extends AppCompatActivity {
             } catch (Exception e) {
                 e.printStackTrace();
             }
-            Log.e("null","This is null");
+            Log.e("null", "This is null");
             return null;
         }
 
@@ -124,14 +134,104 @@ public class MainActivity extends AppCompatActivity {
         }
 
     }
-}
+/*
+//원본
+    public class JSONTask2 extends AsyncTask<String, String, String>{
 
-                    /*
+        @Override
+        protected String doInBackground(String... urls) {
+            try {
+                JSONObject jsonObject = new JSONObject();
+                jsonObject.accumulate("name", "b");
+
+                HttpURLConnection con = null;
+                BufferedReader reader = null;
+
+                try{
+                    //URL url = new URL("http://192.168.25.16:3000/users");
+                    URL url = new URL(urls[0]);
+                    con = (HttpURLConnection) url.openConnection();
+                    con.setRequestMethod("POST");
+                    con.setRequestProperty("Cache-Control", "no-cache");
+                    con.setRequestProperty("Content-Type", "application/json");
+                    con.setRequestProperty("Accept", "text/html");
+                    con.setDoOutput(true);
+                    con.setDoInput(true);
+                    con.connect();
+
+                    OutputStream outStream = con.getOutputStream();
+                    BufferedWriter writer = new BufferedWriter(new OutputStreamWriter(outStream));
+                    writer.write(jsonObject.toString());
+                    writer.flush();
+                    writer.close();
+
+                    InputStream stream = con.getInputStream();
+
+                    reader = new BufferedReader(new InputStreamReader(stream));
+
+                    StringBuffer buffer = new StringBuffer();
+
+                    String line = "";
+                    while((line = reader.readLine()) != null){
+                        buffer.append(line);
+                    }
+
+                    return buffer.toString();
+
+                } catch (MalformedURLException e){
+                    e.printStackTrace();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                } finally {
+                    if(con != null){
+                        con.disconnect();
+                    }
+                    try {
+                        if(reader != null){
+                            reader.close();
+                        }
+                    } catch (IOException e) {
+                        e.printStackTrace();
+                    }
+                }
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+
+            return null;
+        }
+
+        @Override
+        protected void onPostExecute(String result) {
+            super.onPostExecute(result);
+            tvData.setText(result);
+        }
+    }
+*/
+    public class JSONTask2 extends AsyncTask<String, String, String>{
+
+        @Override
+        protected String doInBackground(String... urls) {
+            try {
+                //JSONObject를 만들고 key value 형식으로 값을 저장해준다.
+                JSONObject jsonObject = new JSONObject();
+
+                jsonObject.accumulate("name", "c"); //(2)찾으려는 요소 넣기
+
+
+                HttpURLConnection con = null;
+                BufferedReader reader = null;
+
+                try{
+                    URL url = new URL(urls[0]);
+                    //연결을 함
+                    con = (HttpURLConnection) url.openConnection();
+
+                    con.setRequestProperty("Content-Type", "application/x-www-form-urlencoded");//application JSON 형식으로 전송
                     con.setRequestMethod("POST");//POST방식으로 보냄
                     con.setRequestProperty("Cache-Control", "no-cache");//캐시 설정
-                    con.setRequestProperty("Content-Type", "application/json");//application JSON
 
-                    con.setRequestProperty("Accept", "text/html");//서버에 response 데이터를 html로 받음
+                    con.setRequestProperty("Accept", "application/x-www-form-urlencoded");//서버에 response 데이터를 html로 받음
                     con.setDoOutput(true);//Outstream으로 post 데이터를 넘겨주겠다는 의미
                     con.setDoInput(true);//Inputstream으로 서버로부터 응답을 받겠다는 의미
                     con.connect();
@@ -140,6 +240,7 @@ public class MainActivity extends AppCompatActivity {
                     OutputStream outStream = con.getOutputStream();
                     //버퍼를 생성하고 넣음
                     BufferedWriter writer = new BufferedWriter(new OutputStreamWriter(outStream));
+                    Log.e("Json?",jsonObject.toString());
                     writer.write(jsonObject.toString());
                     writer.flush();
                     writer.close();//버퍼를 받아줌
@@ -152,38 +253,125 @@ public class MainActivity extends AppCompatActivity {
                     StringBuffer buffer = new StringBuffer();
 
                     String line = "";
-                    while ((line = reader.readLine()) != null) {
+                    while((line = reader.readLine()) != null){
                         buffer.append(line);
                     }
 
                     return buffer.toString();//서버로 부터 받은 값을 리턴해줌 아마 OK!!가 들어올것임
 
-                } catch (MalformedURLException e) {
+                } catch (MalformedURLException e){
                     e.printStackTrace();
                 } catch (IOException e) {
                     e.printStackTrace();
                 } finally {
-                    if (con != null) {
+                    if(con != null){
                         con.disconnect();
                     }
                     try {
-                        if (reader != null) {
-                            reader.close();
+                        if(reader != null){
+                            reader.close();//버퍼를 닫아줌
                         }
                     } catch (IOException e) {
                         e.printStackTrace();
                     }
                 }
-            }catch (Exception e){
+            } catch (Exception e) {
                 e.printStackTrace();
             }
+
             return null;
         }
+
         @Override
         protected void onPostExecute(String result) {
             super.onPostExecute(result);
             tvData.setText(result);//서버로 부터 받은 값을 출력해주는 부
         }
     }
+
+    public class JSONTask3 extends AsyncTask<String, String, String>{
+
+        @Override
+        protected String doInBackground(String... urls) {
+            try {
+                //JSONObject를 만들고 key value 형식으로 값을 저장해준다.
+                JSONObject jsonObject = new JSONObject();
+
+                jsonObject.accumulate("name", "insert_name"); //(2)찾으려는 요소 넣기
+                jsonObject.accumulate("address", "insert_address");
+                jsonObject.accumulate("email", "insert_email");
+                jsonObject.accumulate("phone", "insert_phone");
+
+
+
+                HttpURLConnection con = null;
+                BufferedReader reader = null;
+
+                try{
+                    URL url = new URL(urls[0]);
+                    //연결을 함
+                    con = (HttpURLConnection) url.openConnection();
+
+                    con.setRequestProperty("Content-Type", "application/x-www-form-urlencoded");//application JSON 형식으로 전송
+                    con.setRequestMethod("POST");//POST방식으로 보냄
+                    con.setRequestProperty("Cache-Control", "no-cache");//캐시 설정
+
+                    con.setRequestProperty("Accept", "application/x-www-form-urlencoded");//서버에 response 데이터를 html로 받음
+                    con.setDoOutput(true);//Outstream으로 post 데이터를 넘겨주겠다는 의미
+                    con.setDoInput(true);//Inputstream으로 서버로부터 응답을 받겠다는 의미
+                    con.connect();
+
+                    //서버로 보내기위해서 스트림 만듬
+                    OutputStream outStream = con.getOutputStream();
+                    //버퍼를 생성하고 넣음
+                    BufferedWriter writer = new BufferedWriter(new OutputStreamWriter(outStream));
+                    Log.e("Json?",jsonObject.toString());
+                    writer.write(jsonObject.toString());
+                    writer.flush();
+                    writer.close();//버퍼를 받아줌
+
+                    //서버로 부터 데이터를 받음
+                    InputStream stream = con.getInputStream();
+
+                    reader = new BufferedReader(new InputStreamReader(stream));
+
+                    StringBuffer buffer = new StringBuffer();
+
+                    String line = "";
+                    while((line = reader.readLine()) != null){
+                        buffer.append(line);
+                    }
+
+                    return buffer.toString();//서버로 부터 받은 값을 리턴해줌 아마 OK!!가 들어올것임
+
+                } catch (MalformedURLException e){
+                    e.printStackTrace();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                } finally {
+                    if(con != null){
+                        con.disconnect();
+                    }
+                    try {
+                        if(reader != null){
+                            reader.close();//버퍼를 닫아줌
+                        }
+                    } catch (IOException e) {
+                        e.printStackTrace();
+                    }
+                }
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+
+            return null;
+        }
+
+        @Override
+        protected void onPostExecute(String result) {
+            super.onPostExecute(result);
+            tvData.setText(result);//서버로 부터 받은 값을 출력해주는 부
+        }
+    }
+
 }
-*/
--- Nodejs_MySQL_Android/tutorial_index.js
+++ Nodejs_MySQL_Android/tutorial_index.js
@@ -1,3 +1,8 @@
+/*
+RESTFUL Services by NodeJS
+Author: hyunji
+MY SQL SEARCH
+*/
 var express = require('express');
 var mysql = require('mysql');
 var bodyParser = require('body-parser');
@@ -10,9 +15,13 @@ var con = mysql.createConnection({
   database : 'edmstsearch' //이름 변경하기
 });
 
+//CREATE RESTFUL
 var app = express();
+var publicDir = (__dirname+'/public/');
+app.use(express.static(publicDir));
 app.use(bodyParser.json());
 // app.use(bodyParser.urlencoded({extended:true}));
+
 
 
 //Get All PERSON from database
@@ -30,32 +39,9 @@ app.get("/person",(req,res,next)=>{
   });
 });
 
-app.post('/post', (req, res) => {
-   console.log('who get in here post /users');
-   var inputData;
 
-   req.on('data', (data) => {
-     inputData = JSON.parse(data);
-   });
+//SEARCH PERSON by name
 
-   req.on('end', () => {
-     console.log("user_id : "+inputData.user_id + " , name : "+inputData.name);
-     var query = "SELECT * FROM person WHERE name LIKE '%"+inputData.name+"%'";
-
-     con.query(query,function(error,result,fields){
-       con.on('error',function(err){
-         console.log('[MYSQL]ERROR',err);
-       });
-       if(result && result.length){
-         res.end(JSON.stringify(result));
-       }else{
-         res.end(JSON.stringify('NO person here'));
-       }
-     });
-   });
-});
-
-/* 튜토리얼 post
 app.post("/search",(req,res,next)=>{
 
   var post_data = req.body; //GET POST body
@@ -74,30 +60,30 @@ app.post("/search",(req,res,next)=>{
     }
   });
 });
-*/
-app.post('/insert',(req,res)=>{
-  console.log("Here in the /insert");
+
+
+
+/*
+//post added form blog
+app.post('/post', (req, res) => {
+  console.log('who get in here post /users');
+  var inputData;
 
   req.on('data', (data) => {
     inputData = JSON.parse(data);
   });
+
   req.on('end', () => {
-    console.log("name : "+inputData.name + " , address : "+inputData.address+ " , email : "+inputData.email +",phone" + inputData.phone);
-    var insertParams = [inputData.name,inputData.address,inputData.email,inputData.phone];
-    var query = 'INSERT INTO person (name,address,email,phone) VALUES (?,?,?,?)';
-    con.query(query,insertParams,function(err,rows,fields){
-      if(err){
-        console.log(err);
-      }else{
-        console.log("INSERT OK");
-        console.log(rows.insertId);
-      }
-    });
+    console.log("user_id : "+inputData.user_id + " , name : "+inputData.name);
   });
 
+  res.write("OK!");
+  res.end();
 });
+*/
 
 
-app.listen(3306, () => {
-  console.log('Example app listening on port 3306!!');
-});
+//Start SERVER
+app.listen(3306,()=>{
+  console.log('EDMTDev SEARCH API running on port  3306');
+})
